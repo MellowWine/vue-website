98ec851b9bd67a6d3260357ff5b6ed54ed459a95
江韬 <tianjiuweixun@gmail.com>
2025-07-18 11:45:23 +0800

    修改vite配置

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+// vite.config.js
+import { fileURLToPath, URL } from 'node:url'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 在这里添加 base 选项
+  base: 'vue-website', // 替换成你的仓库名！！！
+
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  }),
-)
+  },
+})
